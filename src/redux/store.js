c70a24a6bd5da8521ff7c93b49d182ef90b8c4a1
Aliaksandr Kalanda <74773439+Alex-Kalanda@users.noreply.github.com>
2022-12-29 20:57:43 +0300

    docs: load build

--- src/redux/store.js
+++
@@ -1,4 +0,0 @@
-import { createStore } from 'redux';
-import { todoReducer } from './todoReducer';
-
-export const store = createStore(todoReducer);
--- src/utils/getUniqID.js
+++
@@ -1,5 +0,0 @@
-const getUniqID = () => {
-  return Date.now() + '';
-};
-
-export default getUniqID;
